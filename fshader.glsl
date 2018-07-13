1933a649f8c5b845ec5a6ebc5ab2b2e8b9878e0b
Herman Sletmoen <hermansletmoen@gmail.com>
2018-07-13 14:45:07 +0200

    Added material and light structs to shader

--- fshader.glsl
+++ fshader.glsl
@@ -9,42 +9,45 @@ in vec3 normalF;
 out vec4 fragColor;
 
 // material
-uniform vec3 ambient;
-uniform vec3 diffuse;
-uniform vec3 specular;
-uniform float shine;
-uniform sampler2D ambientMap;
-uniform sampler2D diffuseMap;
-uniform sampler2D specularMap;
-uniform float alpha; // TODO: let textures modify alpha
+uniform struct Material {
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+	float shine;
+	sampler2D ambientMap;
+	sampler2D diffuseMap;
+	sampler2D specularMap;
+	float alpha; // TODO: let textures modify alpha
+} material;
 
-// light
-uniform vec3 lightPosition;
-uniform vec3 ambientLight;
-uniform vec3 diffuseLight;
-uniform vec3 specularLight;
+uniform struct Light {
+	vec3 position;
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+} light;
 
 uniform mat4 viewMatrix;
 
 void main() {
-	vec3 ambientFactor = ambientLight;
-	vec4 ambientMapRGBA = texture(ambientMap, texCoordF);
-	vec3 ambientColor = ambientFactor * ((1 - ambientMapRGBA.a) * ambient + ambientMapRGBA.a * ambientMapRGBA.rgb);
+	vec3 ambientFactor = light.ambient;
+	vec4 ambientMapRGBA = texture(material.ambientMap, texCoordF);
+	vec3 ambientColor = ambientFactor * ((1 - ambientMapRGBA.a) * material.ambient + ambientMapRGBA.a * ambientMapRGBA.rgb);
 
-	vec3 lightDirection = normalize(worldPosition - lightPosition);
+	vec3 lightDirection = normalize(worldPosition - light.position);
 	lightDirection = vec3(viewMatrix * vec4(lightDirection, 0));
 
-	vec3 diffuseFactor = max(dot(normalF, -lightDirection), 0) * diffuseLight;
-	vec4 diffuseMapRGBA = texture(diffuseMap, texCoordF);
-	vec3 diffuseColor = diffuseFactor * ((1 - diffuseMapRGBA.a) * diffuse + diffuseMapRGBA.a * diffuseMapRGBA.rgb);
+	vec3 diffuseFactor = max(dot(normalF, -lightDirection), 0) * light.diffuse;
+	vec4 diffuseMapRGBA = texture(material.diffuseMap, texCoordF);
+	vec3 diffuseColor = diffuseFactor * ((1 - diffuseMapRGBA.a) * material.diffuse + diffuseMapRGBA.a * diffuseMapRGBA.rgb);
 
 	vec3 reflection = reflect(lightDirection, normalF);
 	vec3 fragDirection = normalize(viewPosition) - vec3(0, 0, 0);
 	float facing = dot(normalF, lightDirection) < 0 ? 1 : 0;
-	vec3 specularFactor = max(pow(dot(reflection, -fragDirection), shine), 0) *
-	                      facing * specularLight;
-	vec4 specularMapRGBA = texture(specularMap, texCoordF);
-	vec3 specularColor = specularFactor * ((1 - specularMapRGBA.a) * specular + specularMapRGBA.a * specularMapRGBA.rgb);
+	vec3 specularFactor = max(pow(dot(reflection, -fragDirection), material.shine), 0) *
+	                      facing * light.specular;
+	vec4 specularMapRGBA = texture(material.specularMap, texCoordF);
+	vec3 specularColor = specularFactor * ((1 - specularMapRGBA.a) * material.specular + specularMapRGBA.a * specularMapRGBA.rgb);
 
-	fragColor = vec4(ambientColor + diffuseColor + specularColor, alpha);
+	fragColor = vec4(ambientColor + diffuseColor + specularColor, material.alpha);
 }
