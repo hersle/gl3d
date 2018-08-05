db0024cf3d252eb801fe6b324df4dce0b9e557af
Herman Sletmoen <hermansletmoen@gmail.com>
2018-08-06 00:34:24 +0200

    Improved box and sphere geometry types

--- geometry.go
+++ geometry.go
@@ -1,25 +1,56 @@
 package main
 
+import (
+	"main"
+)
+
 type Box struct {
-	width float32
-	height float32
-	depth float32
+	Min Vec3
+	Max Vec3
 }
 
 type Sphere struct {
-	radius float32
+	Center Vec3
+	Radius float32
 }
 
-func NewBox(width, height, depth float32) *Box {
+func NewBox(point1, point2 Vec3) *Box {
 	var b Box
-	b.width = width
-	b.height = height
-	b.depth = depth
+
+	minX := math.Min(point1.X(), point2.X())
+	minY := math.Min(point1.Y(), point2.Y())
+	minZ := math.Min(point1.Z(), point2.Z())
+	b.Min = NewVec3(minX, minY, minZ)
+
+	maxX := math.Max(point1.X(), point2.X())
+	maxY := math.Max(point1.Y(), point2.Y())
+	maxZ := math.Max(point1.Z(), point2.Z())
+	b.Max = NewVec3(maxX, maxY, maxZ)
+
 	return &b
 }
 
-func NewSphere(radius float32) *Sphere {
+func (b *Box) Dx() float32 {
+	return b.Max.X() - b.Min.X()
+}
+
+func (b *Box) Dy() float32 {
+	return b.Max.Y() - b.Min.Y()
+}
+
+func (b *Box) Dz() float32 {
+	return b.Max.Z() - b.Min.Z()
+}
+
+func (b *Box) Center() Vec3 {
+	return b.Min.Add(b.Max).Scale(0.5)
+}
+
+func NewSphere(center Vec3, radius float32) *Sphere {
 	var s Sphere
-	s.radius = radius
+
+	s.Center = center
+	s.Radius = radius
+
 	return &s
 }
