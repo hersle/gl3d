7d2a215dc6003f3bc94bdb568879c3e3ab65b972
Herman Sletmoen <hermansletmoen@gmail.com>
2018-08-06 16:09:35 +0200

    Exported shader variables

--- shader.go
+++ shader.go
@@ -9,81 +9,81 @@ import (
 type MeshShaderProgram struct {
 	*ShaderProgram
 
-	position *Attrib
-	texCoord *Attrib
-	normal *Attrib
-	tangent *Attrib
+	Position *Attrib
+	TexCoord *Attrib
+	Normal *Attrib
+	Tangent *Attrib
 
-	modelMatrix *UniformMatrix4
-	viewMatrix *UniformMatrix4
-	projectionMatrix *UniformMatrix4
+	ModelMatrix *UniformMatrix4
+	ViewMatrix *UniformMatrix4
+	ProjectionMatrix *UniformMatrix4
 
-	ambient *UniformVector3
-	ambientMap *UniformSampler
-	diffuse *UniformVector3
-	diffuseMap *UniformSampler
-	specular *UniformVector3
-	specularMap *UniformSampler
-	shine *UniformFloat
-	alpha *UniformFloat
-	alphaMap *UniformSampler
-	hasAlphaMap *UniformBool
-	bumpMap *UniformSampler
-	hasBumpMap *UniformBool
+	Ambient *UniformVector3
+	AmbientMap *UniformSampler
+	Diffuse *UniformVector3
+	DiffuseMap *UniformSampler
+	Specular *UniformVector3
+	SpecularMap *UniformSampler
+	Shine *UniformFloat
+	Alpha *UniformFloat
+	AlphaMap *UniformSampler
+	HasAlphaMap *UniformBool
+	BumpMap *UniformSampler
+	HasBumpMap *UniformBool
 
-	lightType *UniformInteger
-	lightPos *UniformVector3
-	lightDir *UniformVector3
-	ambientLight *UniformVector3
-	diffuseLight *UniformVector3
-	specularLight *UniformVector3
-	shadowViewMatrix *UniformMatrix4
-	shadowProjectionMatrix *UniformMatrix4
-	spotShadowMap *UniformSampler
-	cubeShadowMap *UniformSampler
-	shadowFar *UniformFloat
+	LightType *UniformInteger
+	LightPos *UniformVector3
+	LightDir *UniformVector3
+	AmbientLight *UniformVector3
+	DiffuseLight *UniformVector3
+	SpecularLight *UniformVector3
+	ShadowViewMatrix *UniformMatrix4
+	ShadowProjectionMatrix *UniformMatrix4
+	SpotShadowMap *UniformSampler
+	CubeShadowMap *UniformSampler
+	ShadowFar *UniformFloat
 }
 
 type SkyboxShaderProgram struct {
 	*ShaderProgram
-	viewMatrix *UniformMatrix4
-	projectionMatrix *UniformMatrix4
-	cubeMap *UniformSampler
-	position *Attrib
+	ViewMatrix *UniformMatrix4
+	ProjectionMatrix *UniformMatrix4
+	CubeMap *UniformSampler
+	Position *Attrib
 }
 
 type TextShaderProgram struct {
 	*ShaderProgram
-	atlas *UniformSampler
-	position *Attrib
-	texCoord *Attrib
+	Atlas *UniformSampler
+	Position *Attrib
+	TexCoord *Attrib
 }
 
 type ShadowMapShaderProgram struct {
 	*ShaderProgram
-	modelMatrix *UniformMatrix4
-	viewMatrix *UniformMatrix4
-	projectionMatrix *UniformMatrix4
-	lightPosition *UniformVector3
-	far *UniformFloat
-	position *Attrib
+	ModelMatrix *UniformMatrix4
+	ViewMatrix *UniformMatrix4
+	ProjectionMatrix *UniformMatrix4
+	LightPosition *UniformVector3
+	Far *UniformFloat
+	Position *Attrib
 }
 
 type ArrowShaderProgram struct {
 	*ShaderProgram
-	modelMatrix *UniformMatrix4
-	viewMatrix *UniformMatrix4
-	projectionMatrix *UniformMatrix4
-	color *UniformVector3
-	position *Attrib
+	ModelMatrix *UniformMatrix4
+	ViewMatrix *UniformMatrix4
+	ProjectionMatrix *UniformMatrix4
+	Color *UniformVector3
+	Position *Attrib
 }
 
 type DepthPassShaderProgram struct {
 	*ShaderProgram
-	modelMatrix *UniformMatrix4
-	viewMatrix *UniformMatrix4
-	projectionMatrix *UniformMatrix4
-	position *Attrib
+	ModelMatrix *UniformMatrix4
+	ViewMatrix *UniformMatrix4
+	ProjectionMatrix *UniformMatrix4
+	Position *Attrib
 }
 
 func NewMeshShaderProgram() *MeshShaderProgram {
@@ -98,115 +98,115 @@ func NewMeshShaderProgram() *MeshShaderProgram {
 		panic("error loading mesh shader")
 	}
 
-	sp.position = sp.Attrib("position")
-	sp.texCoord = sp.Attrib("texCoordV")
-	sp.normal = sp.Attrib("normalV")
-	sp.tangent = sp.Attrib("tangentV")
+	sp.Position = sp.Attrib("position")
+	sp.TexCoord = sp.Attrib("texCoordV")
+	sp.Normal = sp.Attrib("normalV")
+	sp.Tangent = sp.Attrib("tangentV")
 
-	sp.modelMatrix = sp.UniformMatrix4("modelMatrix")
-	sp.viewMatrix = sp.UniformMatrix4("viewMatrix")
-	sp.projectionMatrix = sp.UniformMatrix4("projectionMatrix")
+	sp.ModelMatrix = sp.UniformMatrix4("modelMatrix")
+	sp.ViewMatrix = sp.UniformMatrix4("viewMatrix")
+	sp.ProjectionMatrix = sp.UniformMatrix4("projectionMatrix")
 
-	sp.ambient = sp.UniformVector3("material.ambient")
-	sp.ambientMap = sp.UniformSampler("material.ambientMap")
-	sp.diffuse = sp.UniformVector3("material.diffuse")
-	sp.diffuseMap = sp.UniformSampler("material.diffuseMap")
-	sp.specular = sp.UniformVector3("material.specular")
-	sp.specularMap = sp.UniformSampler("material.specularMap")
-	sp.shine = sp.UniformFloat("material.shine")
-	sp.alpha = sp.UniformFloat("material.alpha")
-	sp.alphaMap = sp.UniformSampler("material.alphaMap")
-	sp.hasAlphaMap = sp.UniformBool("material.hasAlphaMap")
-	sp.bumpMap = sp.UniformSampler("material.bumpMap")
-	sp.hasBumpMap = sp.UniformBool("material.hasBumpMap")
+	sp.Ambient = sp.UniformVector3("material.ambient")
+	sp.AmbientMap = sp.UniformSampler("material.ambientMap")
+	sp.Diffuse = sp.UniformVector3("material.diffuse")
+	sp.DiffuseMap = sp.UniformSampler("material.diffuseMap")
+	sp.Specular = sp.UniformVector3("material.specular")
+	sp.SpecularMap = sp.UniformSampler("material.specularMap")
+	sp.Shine = sp.UniformFloat("material.shine")
+	sp.Alpha = sp.UniformFloat("material.alpha")
+	sp.AlphaMap = sp.UniformSampler("material.alphaMap")
+	sp.HasAlphaMap = sp.UniformBool("material.hasAlphaMap")
+	sp.BumpMap = sp.UniformSampler("material.bumpMap")
+	sp.HasBumpMap = sp.UniformBool("material.hasBumpMap")
 
-	sp.lightType = sp.UniformInteger("light.type")
-	sp.lightPos = sp.UniformVector3("light.position")
-	sp.lightDir = sp.UniformVector3("light.direction")
-	sp.ambientLight = sp.UniformVector3("light.ambient")
-	sp.diffuseLight = sp.UniformVector3("light.diffuse")
-	sp.specularLight = sp.UniformVector3("light.specular")
-	sp.shadowViewMatrix = sp.UniformMatrix4("shadowViewMatrix")
-	sp.shadowProjectionMatrix = sp.UniformMatrix4("shadowProjectionMatrix")
-	sp.cubeShadowMap = sp.UniformSampler("cubeShadowMap")
-	sp.spotShadowMap = sp.UniformSampler("spotShadowMap")
-	sp.shadowFar = sp.UniformFloat("light.far")
+	sp.LightType = sp.UniformInteger("light.type")
+	sp.LightPos = sp.UniformVector3("light.position")
+	sp.LightDir = sp.UniformVector3("light.direction")
+	sp.AmbientLight = sp.UniformVector3("light.ambient")
+	sp.DiffuseLight = sp.UniformVector3("light.diffuse")
+	sp.SpecularLight = sp.UniformVector3("light.specular")
+	sp.ShadowViewMatrix = sp.UniformMatrix4("shadowViewMatrix")
+	sp.ShadowProjectionMatrix = sp.UniformMatrix4("shadowProjectionMatrix")
+	sp.CubeShadowMap = sp.UniformSampler("cubeShadowMap")
+	sp.SpotShadowMap = sp.UniformSampler("spotShadowMap")
+	sp.ShadowFar = sp.UniformFloat("light.far")
 
-	sp.position.SetFormat(gl.FLOAT, false)
-	sp.normal.SetFormat(gl.FLOAT, false)
-	sp.texCoord.SetFormat(gl.FLOAT, false)
-	sp.tangent.SetFormat(gl.FLOAT, false)
+	sp.Position.SetFormat(gl.FLOAT, false)
+	sp.Normal.SetFormat(gl.FLOAT, false)
+	sp.TexCoord.SetFormat(gl.FLOAT, false)
+	sp.Tangent.SetFormat(gl.FLOAT, false)
 
 	return &sp
 }
 
 func (sp *MeshShaderProgram) SetCamera(c *Camera) {
-	sp.viewMatrix.Set(c.ViewMatrix())
-	sp.projectionMatrix.Set(c.ProjectionMatrix())
+	sp.ViewMatrix.Set(c.ViewMatrix())
+	sp.ProjectionMatrix.Set(c.ProjectionMatrix())
 }
 
 func (sp *MeshShaderProgram) SetMesh(m *Mesh) {
-	sp.modelMatrix.Set(m.WorldMatrix())
+	sp.ModelMatrix.Set(m.WorldMatrix())
 }
 
 func (sp *MeshShaderProgram) SetSubMesh(sm *SubMesh) {
 	mtl := sm.mtl
 
-	sp.ambient.Set(mtl.ambient)
-	sp.ambientMap.Set2D(mtl.ambientMap)
-	sp.diffuse.Set(mtl.diffuse)
-	sp.diffuseMap.Set2D(mtl.diffuseMap)
-	sp.specular.Set(mtl.specular)
-	sp.specularMap.Set2D(mtl.specularMap)
-	sp.shine.Set(mtl.shine)
-	sp.alpha.Set(mtl.alpha)
+	sp.Ambient.Set(mtl.ambient)
+	sp.AmbientMap.Set2D(mtl.ambientMap)
+	sp.Diffuse.Set(mtl.diffuse)
+	sp.DiffuseMap.Set2D(mtl.diffuseMap)
+	sp.Specular.Set(mtl.specular)
+	sp.SpecularMap.Set2D(mtl.specularMap)
+	sp.Shine.Set(mtl.shine)
+	sp.Alpha.Set(mtl.alpha)
 
 	if mtl.HasAlphaMap() {
-		sp.hasAlphaMap.Set(true)
-		sp.alphaMap.Set2D(mtl.alphaMap)
+		sp.HasAlphaMap.Set(true)
+		sp.AlphaMap.Set2D(mtl.alphaMap)
 	} else {
-		sp.hasAlphaMap.Set(false)
+		sp.HasAlphaMap.Set(false)
 	}
 
 	if mtl.HasBumpMap() {
-		sp.hasBumpMap.Set(true)
-		sp.bumpMap.Set2D(mtl.bumpMap)
+		sp.HasBumpMap.Set(true)
+		sp.BumpMap.Set2D(mtl.bumpMap)
 	} else {
-		sp.hasBumpMap.Set(false)
+		sp.HasBumpMap.Set(false)
 	}
 
 	var v Vertex
-	sp.position.SetSource(sm.vbo, v.PositionOffset(), v.Size())
-	sp.normal.SetSource(sm.vbo, v.NormalOffset(), v.Size())
-	sp.texCoord.SetSource(sm.vbo, v.TexCoordOffset(), v.Size())
-	sp.tangent.SetSource(sm.vbo, v.TangentOffset(), v.Size())
+	sp.Position.SetSource(sm.vbo, v.PositionOffset(), v.Size())
+	sp.Normal.SetSource(sm.vbo, v.NormalOffset(), v.Size())
+	sp.TexCoord.SetSource(sm.vbo, v.TexCoordOffset(), v.Size())
+	sp.Tangent.SetSource(sm.vbo, v.TangentOffset(), v.Size())
 	sp.SetAttribIndexBuffer(sm.ibo)
 }
 
 func (sp *MeshShaderProgram) SetAmbientLight(l *AmbientLight) {
-	sp.lightType.Set(0)
-	sp.ambientLight.Set(l.color)
+	sp.LightType.Set(0)
+	sp.AmbientLight.Set(l.color)
 }
 
 func (sp *MeshShaderProgram) SetPointLight(l *PointLight) {
-	sp.lightType.Set(1)
-	sp.lightPos.Set(l.position)
-	sp.diffuseLight.Set(l.diffuse)
-	sp.specularLight.Set(l.specular)
-	sp.cubeShadowMap.SetCube(l.shadowMap)
-	sp.shadowFar.Set(l.shadowFar)
+	sp.LightType.Set(1)
+	sp.LightPos.Set(l.position)
+	sp.DiffuseLight.Set(l.diffuse)
+	sp.SpecularLight.Set(l.specular)
+	sp.CubeShadowMap.SetCube(l.shadowMap)
+	sp.ShadowFar.Set(l.shadowFar)
 }
 
 func (sp *MeshShaderProgram) SetSpotLight(l *SpotLight) {
-	sp.lightType.Set(2)
-	sp.lightPos.Set(l.position)
-	sp.lightDir.Set(l.Forward())
-	sp.diffuseLight.Set(l.diffuse)
-	sp.specularLight.Set(l.specular)
-	sp.spotShadowMap.Set2D(l.shadowMap)
-	sp.shadowViewMatrix.Set(l.ViewMatrix())
-	sp.shadowProjectionMatrix.Set(l.ProjectionMatrix())
-	sp.shadowFar.Set(l.Camera.far)
+	sp.LightType.Set(2)
+	sp.LightPos.Set(l.position)
+	sp.LightDir.Set(l.Forward())
+	sp.DiffuseLight.Set(l.diffuse)
+	sp.SpecularLight.Set(l.specular)
+	sp.SpotShadowMap.Set2D(l.shadowMap)
+	sp.ShadowViewMatrix.Set(l.ViewMatrix())
+	sp.ShadowProjectionMatrix.Set(l.ProjectionMatrix())
+	sp.ShadowFar.Set(l.Camera.far)
 }
 
 func NewSkyboxShaderProgram() *SkyboxShaderProgram {
@@ -221,26 +221,26 @@ func NewSkyboxShaderProgram() *SkyboxShaderProgram {
 		panic(err)
 	}
 
-	sp.viewMatrix = sp.UniformMatrix4("viewMatrix")
-	sp.projectionMatrix = sp.UniformMatrix4("projectionMatrix")
-	sp.cubeMap = sp.UniformSampler("cubeMap")
-	sp.position = sp.Attrib("positionV")
+	sp.ViewMatrix = sp.UniformMatrix4("viewMatrix")
+	sp.ProjectionMatrix = sp.UniformMatrix4("projectionMatrix")
+	sp.CubeMap = sp.UniformSampler("cubeMap")
+	sp.Position = sp.Attrib("positionV")
 
 	return &sp
 }
 
 func (sp *SkyboxShaderProgram) SetCamera(c *Camera) {
-	sp.viewMatrix.Set(c.ViewMatrix())
-	sp.projectionMatrix.Set(c.ProjectionMatrix())
+	sp.ViewMatrix.Set(c.ViewMatrix())
+	sp.ProjectionMatrix.Set(c.ProjectionMatrix())
 }
 
 func (sp *SkyboxShaderProgram) SetSkybox(skybox *CubeMap) {
-	sp.cubeMap.SetCube(skybox)
+	sp.CubeMap.SetCube(skybox)
 }
 
 func (sp *SkyboxShaderProgram) SetCube(vbo, ibo *Buffer) {
-	sp.position.SetFormat(gl.FLOAT, false)
-	sp.position.SetSource(vbo, 0, int(unsafe.Sizeof(math.NewVec3(0, 0, 0))))
+	sp.Position.SetFormat(gl.FLOAT, false)
+	sp.Position.SetSource(vbo, 0, int(unsafe.Sizeof(math.NewVec3(0, 0, 0))))
 	sp.SetAttribIndexBuffer(ibo)
 }
 
@@ -255,24 +255,24 @@ func NewTextShaderProgram() *TextShaderProgram {
 		panic(err)
 	}
 
-	sp.atlas = sp.UniformSampler("fontAtlas")
-	sp.position = sp.Attrib("position")
-	sp.texCoord = sp.Attrib("texCoordV")
+	sp.Atlas = sp.UniformSampler("fontAtlas")
+	sp.Position = sp.Attrib("position")
+	sp.TexCoord = sp.Attrib("texCoordV")
 
-	sp.position.SetFormat(gl.FLOAT, false)
-	sp.texCoord.SetFormat(gl.FLOAT, false)
+	sp.Position.SetFormat(gl.FLOAT, false)
+	sp.TexCoord.SetFormat(gl.FLOAT, false)
 
 	return &sp
 }
 
 func (sp *TextShaderProgram) SetAtlas(tex *Texture2D) {
-	sp.atlas.Set2D(tex)
+	sp.Atlas.Set2D(tex)
 }
 
 func (sp *TextShaderProgram) SetAttribs(vbo, ibo *Buffer) {
 	var v Vertex
-	sp.position.SetSource(vbo, v.PositionOffset(), v.Size())
-	sp.texCoord.SetSource(vbo, v.TexCoordOffset(), v.Size())
+	sp.Position.SetSource(vbo, v.PositionOffset(), v.Size())
+	sp.TexCoord.SetSource(vbo, v.TexCoordOffset(), v.Size())
 	sp.SetAttribIndexBuffer(ibo)
 }
 
@@ -287,32 +287,32 @@ func NewShadowMapShaderProgram() *ShadowMapShaderProgram {
 		panic(err)
 	}
 
-	sp.modelMatrix = sp.UniformMatrix4("modelMatrix")
-	sp.viewMatrix = sp.UniformMatrix4("viewMatrix")
-	sp.projectionMatrix = sp.UniformMatrix4("projectionMatrix")
-	sp.lightPosition = sp.UniformVector3("lightPosition")
-	sp.far = sp.UniformFloat("far")
-	sp.position = sp.Attrib("position")
+	sp.ModelMatrix = sp.UniformMatrix4("modelMatrix")
+	sp.ViewMatrix = sp.UniformMatrix4("viewMatrix")
+	sp.ProjectionMatrix = sp.UniformMatrix4("projectionMatrix")
+	sp.LightPosition = sp.UniformVector3("lightPosition")
+	sp.Far = sp.UniformFloat("far")
+	sp.Position = sp.Attrib("position")
 
-	sp.position.SetFormat(gl.FLOAT, false)
+	sp.Position.SetFormat(gl.FLOAT, false)
 
 	return &sp
 }
 
 func (sp *ShadowMapShaderProgram) SetCamera(c *Camera) {
-	sp.far.Set(c.far)
-	sp.lightPosition.Set(c.position)
-	sp.viewMatrix.Set(c.ViewMatrix())
-	sp.projectionMatrix.Set(c.ProjectionMatrix())
+	sp.Far.Set(c.far)
+	sp.LightPosition.Set(c.position)
+	sp.ViewMatrix.Set(c.ViewMatrix())
+	sp.ProjectionMatrix.Set(c.ProjectionMatrix())
 }
 
 func (sp *ShadowMapShaderProgram) SetMesh(m *Mesh) {
-	sp.modelMatrix.Set(m.WorldMatrix())
+	sp.ModelMatrix.Set(m.WorldMatrix())
 }
 
 func (sp *ShadowMapShaderProgram) SetSubMesh(sm *SubMesh) {
 	var v Vertex
-	sp.position.SetSource(sm.vbo, v.PositionOffset(), v.Size())
+	sp.Position.SetSource(sm.vbo, v.PositionOffset(), v.Size())
 	sp.SetAttribIndexBuffer(sm.ibo)
 }
 
@@ -327,33 +327,33 @@ func NewArrowShaderProgram() *ArrowShaderProgram {
 		panic(err)
 	}
 
-	sp.position = sp.Attrib("position")
-	sp.modelMatrix = sp.UniformMatrix4("modelMatrix")
-	sp.viewMatrix = sp.UniformMatrix4("viewMatrix")
-	sp.projectionMatrix = sp.UniformMatrix4("projectionMatrix")
-	sp.color = sp.UniformVector3("color")
+	sp.Position = sp.Attrib("position")
+	sp.ModelMatrix = sp.UniformMatrix4("modelMatrix")
+	sp.ViewMatrix = sp.UniformMatrix4("viewMatrix")
+	sp.ProjectionMatrix = sp.UniformMatrix4("projectionMatrix")
+	sp.Color = sp.UniformVector3("color")
 
-	sp.position.SetFormat(gl.FLOAT, false)
+	sp.Position.SetFormat(gl.FLOAT, false)
 
 	return &sp
 }
 
 func (sp *ArrowShaderProgram) SetCamera(c *Camera) {
-	sp.viewMatrix.Set(c.ViewMatrix())
-	sp.projectionMatrix.Set(c.ProjectionMatrix())
+	sp.ViewMatrix.Set(c.ViewMatrix())
+	sp.ProjectionMatrix.Set(c.ProjectionMatrix())
 }
 
 func (sp *ArrowShaderProgram) SetMesh(m *Mesh) {
-	sp.modelMatrix.Set(m.WorldMatrix())
+	sp.ModelMatrix.Set(m.WorldMatrix())
 }
 
 func (sp *ArrowShaderProgram) SetColor(color math.Vec3) {
-	sp.color.Set(color)
+	sp.Color.Set(color)
 }
 
 func (sp *ArrowShaderProgram) SetPosition(vbo *Buffer) {
 	stride := int(unsafe.Sizeof(math.NewVec3(0, 0, 0)))
-	sp.position.SetSource(vbo, 0, stride)
+	sp.Position.SetSource(vbo, 0, stride)
 }
 
 func NewDepthPassShaderProgram() *DepthPassShaderProgram {
@@ -366,26 +366,26 @@ func NewDepthPassShaderProgram() *DepthPassShaderProgram {
 		panic(err)
 	}
 
-	sp.position = sp.Attrib("position")
-	sp.modelMatrix = sp.UniformMatrix4("modelMatrix")
-	sp.viewMatrix = sp.UniformMatrix4("viewMatrix")
-	sp.projectionMatrix = sp.UniformMatrix4("projectionMatrix")
-	sp.position.SetFormat(gl.FLOAT, false)
+	sp.Position = sp.Attrib("position")
+	sp.ModelMatrix = sp.UniformMatrix4("modelMatrix")
+	sp.ViewMatrix = sp.UniformMatrix4("viewMatrix")
+	sp.ProjectionMatrix = sp.UniformMatrix4("projectionMatrix")
+	sp.Position.SetFormat(gl.FLOAT, false)
 
 	return &sp
 }
 
 func (sp *DepthPassShaderProgram) SetCamera(c *Camera) {
-	sp.viewMatrix.Set(c.ViewMatrix())
-	sp.projectionMatrix.Set(c.ProjectionMatrix())
+	sp.ViewMatrix.Set(c.ViewMatrix())
+	sp.ProjectionMatrix.Set(c.ProjectionMatrix())
 }
 
 func (sp *DepthPassShaderProgram) SetMesh(m *Mesh) {
-	sp.modelMatrix.Set(m.WorldMatrix())
+	sp.ModelMatrix.Set(m.WorldMatrix())
 }
 
 func (sp *DepthPassShaderProgram) SetSubMesh(sm *SubMesh) {
 	var v Vertex
-	sp.position.SetSource(sm.vbo, v.PositionOffset(), v.Size())
+	sp.Position.SetSource(sm.vbo, v.PositionOffset(), v.Size())
 	sp.SetAttribIndexBuffer(sm.ibo)
 }
