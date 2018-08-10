028ad41ad965b8d5ae1965bac63e753f1a02f29d
Herman Sletmoen <hermansletmoen@gmail.com>
2018-08-10 17:23:59 +0200

    Avoid redundant render state changes with current render state cache

--- graphics/state.go
+++ graphics/state.go
@@ -2,7 +2,7 @@ package graphics
 
 import (
 	"github.com/go-gl/gl/v4.5-core/gl"
-	_ "github.com/hersle/gl3d/window" // initialize graphics
+	"github.com/hersle/gl3d/window" // initialize graphics
 )
 
 type DepthTest int
@@ -59,6 +59,8 @@ type RenderState struct {
 	TriangleMode   TriangleMode
 }
 
+var currentState RenderState
+
 func NewRenderState() *RenderState {
 	var rs RenderState
 	rs.DisableBlending()
@@ -72,108 +74,140 @@ func (rs *RenderState) DisableBlending() {
 }
 
 func (rs *RenderState) Apply() {
-	switch rs.Program {
-	case nil:
-		panic("tried to apply a render state with no shader program")
-	default:
-		rs.Program.va.Bind()
-		rs.Program.Bind()
-	}
-
-	switch rs.Framebuffer {
-	case nil:
-		panic("tried to apply a render state with no framebuffer")
-	default:
-		rs.Framebuffer.BindDraw()
-	}
-
-	switch (rs.DepthTest) {
-	case NeverDepthTest:
-		gl.Enable(gl.DEPTH_TEST)
-		gl.DepthFunc(gl.NEVER)
-	case LessDepthTest:
-		gl.Enable(gl.DEPTH_TEST)
-		gl.DepthFunc(gl.LESS)
-	case LessEqualDepthTest:
-		gl.Enable(gl.DEPTH_TEST)
-		gl.DepthFunc(gl.LEQUAL)
-	case EqualDepthTest:
-		gl.Enable(gl.DEPTH_TEST)
-		gl.DepthFunc(gl.EQUAL)
-	case NotEqualDepthTest:
-		gl.Enable(gl.DEPTH_TEST)
-		gl.DepthFunc(gl.NOTEQUAL)
-	case GreaterDepthTest:
-		gl.Enable(gl.DEPTH_TEST)
-		gl.DepthFunc(gl.GREATER)
-	case GreaterEqualDepthTest:
-		gl.Enable(gl.DEPTH_TEST)
-		gl.DepthFunc(gl.GEQUAL)
-	case AlwaysDepthTest:
-		gl.Disable(gl.DEPTH_TEST)
-	default:
-		panic("tried to apply a render state with unknown depth test")
-	}
-
-	var factors [2]BlendFactor
-	var funcs [2]uint32
-	factors[0] = rs.BlendSourceFactor
-	factors[1] = rs.BlendDestinationFactor
-	for i := 0; i < 2; i++ {
-		switch factors[i] {
-		case ZeroBlendFactor:
-			funcs[i] = gl.ZERO
-		case OneBlendFactor:
-			funcs[i] = gl.ONE
-		case SourceColorBlendFactor:
-			funcs[i] = gl.SRC_COLOR
-		case OneMinusSourceColorBlendFactor:
-			funcs[i] = gl.ONE_MINUS_SRC_COLOR
-		case DestinationColorBlendFactor:
-			funcs[i] = gl.DST_COLOR
-		case OneMinusDestinationColorBlendFactor:
-			funcs[i] = gl.ONE_MINUS_DST_COLOR
-		case SourceAlphaBlendFactor:
-			funcs[i] = gl.SRC_ALPHA
-		case OneMinusSourceAlphaBlendFactor:
-			funcs[i] = gl.ONE_MINUS_SRC_ALPHA
-		case DestinationAlphaBlendFactor:
-			funcs[i] = gl.DST_ALPHA
-		case OneMinusDestinationAlphaBlendFactor:
-			funcs[i] = gl.ONE_MINUS_DST_ALPHA
+	if currentState.Program != rs.Program {
+		switch rs.Program {
+		case nil:
+			panic("tried to apply a render state with no shader program")
 		default:
-			panic("tried to apply a render state with an unknown blending factor")
+			rs.Program.va.Bind()
+			rs.Program.Bind()
 		}
-	}
-	gl.BlendFunc(funcs[0], funcs[1])
-
-	switch rs.Cull {
-	case CullNothing:
-		gl.Disable(gl.CULL_FACE)
-	case CullFront:
-		gl.Enable(gl.CULL_FACE)
-		gl.CullFace(gl.FRONT)
-	case CullBack:
-		gl.Enable(gl.CULL_FACE)
-		gl.CullFace(gl.BACK)
-	default:
-		panic("tried to apply a render state with an unknown culling mode")
+		currentState.Program = rs.Program
 	}
 
-	switch rs.TriangleMode {
-	case PointTriangleMode:
-		gl.PolygonMode(gl.FRONT_AND_BACK, gl.POINT)
-	case LineTriangleMode:
-		gl.PolygonMode(gl.FRONT_AND_BACK, gl.LINE)
-	case TriangleTriangleMode:
-		gl.PolygonMode(gl.FRONT_AND_BACK, gl.FILL)
-	default:
-		panic("tried to apply a render state with an unknown polygonmode")
+	if currentState.Framebuffer != rs.Framebuffer {
+		switch rs.Framebuffer {
+		case nil:
+			panic("tried to apply a render state with no framebuffer")
+		default:
+			rs.Framebuffer.BindDraw()
+		}
+		currentState.Framebuffer = rs.Framebuffer
 	}
 
-	gl.Viewport(0, 0, int32(rs.ViewportWidth), int32(rs.ViewportHeight))
+	if currentState.DepthTest != rs.DepthTest {
+		switch (rs.DepthTest) {
+		case NeverDepthTest:
+			gl.Enable(gl.DEPTH_TEST)
+			gl.DepthFunc(gl.NEVER)
+		case LessDepthTest:
+			gl.Enable(gl.DEPTH_TEST)
+			gl.DepthFunc(gl.LESS)
+		case LessEqualDepthTest:
+			gl.Enable(gl.DEPTH_TEST)
+			gl.DepthFunc(gl.LEQUAL)
+		case EqualDepthTest:
+			gl.Enable(gl.DEPTH_TEST)
+			gl.DepthFunc(gl.EQUAL)
+		case NotEqualDepthTest:
+			gl.Enable(gl.DEPTH_TEST)
+			gl.DepthFunc(gl.NOTEQUAL)
+		case GreaterDepthTest:
+			gl.Enable(gl.DEPTH_TEST)
+			gl.DepthFunc(gl.GREATER)
+		case GreaterEqualDepthTest:
+			gl.Enable(gl.DEPTH_TEST)
+			gl.DepthFunc(gl.GEQUAL)
+		case AlwaysDepthTest:
+			gl.Disable(gl.DEPTH_TEST)
+		default:
+			panic("tried to apply a render state with unknown depth test")
+		}
+		currentState.DepthTest = rs.DepthTest
+	}
+
+	if currentState.BlendSourceFactor != rs.BlendSourceFactor || currentState.BlendDestinationFactor != rs.BlendDestinationFactor {
+		var factors [2]BlendFactor
+		var funcs [2]uint32
+		factors[0] = rs.BlendSourceFactor
+		factors[1] = rs.BlendDestinationFactor
+		for i := 0; i < 2; i++ {
+			switch factors[i] {
+			case ZeroBlendFactor:
+				funcs[i] = gl.ZERO
+			case OneBlendFactor:
+				funcs[i] = gl.ONE
+			case SourceColorBlendFactor:
+				funcs[i] = gl.SRC_COLOR
+			case OneMinusSourceColorBlendFactor:
+				funcs[i] = gl.ONE_MINUS_SRC_COLOR
+			case DestinationColorBlendFactor:
+				funcs[i] = gl.DST_COLOR
+			case OneMinusDestinationColorBlendFactor:
+				funcs[i] = gl.ONE_MINUS_DST_COLOR
+			case SourceAlphaBlendFactor:
+				funcs[i] = gl.SRC_ALPHA
+			case OneMinusSourceAlphaBlendFactor:
+				funcs[i] = gl.ONE_MINUS_SRC_ALPHA
+			case DestinationAlphaBlendFactor:
+				funcs[i] = gl.DST_ALPHA
+			case OneMinusDestinationAlphaBlendFactor:
+				funcs[i] = gl.ONE_MINUS_DST_ALPHA
+			default:
+				panic("tried to apply a render state with an unknown blending factor")
+			}
+		}
+		gl.BlendFunc(funcs[0], funcs[1])
+		currentState.BlendSourceFactor = rs.BlendSourceFactor
+		currentState.BlendDestinationFactor = rs.BlendDestinationFactor
+	}
+
+	if currentState.Cull != rs.Cull {
+		switch rs.Cull {
+		case CullNothing:
+			gl.Disable(gl.CULL_FACE)
+		case CullFront:
+			gl.Enable(gl.CULL_FACE)
+			gl.CullFace(gl.FRONT)
+		case CullBack:
+			gl.Enable(gl.CULL_FACE)
+			gl.CullFace(gl.BACK)
+		default:
+			panic("tried to apply a render state with an unknown culling mode")
+		}
+		currentState.Cull = rs.Cull
+	}
+
+	if currentState.TriangleMode != rs.TriangleMode {
+		switch rs.TriangleMode {
+		case PointTriangleMode:
+			gl.PolygonMode(gl.FRONT_AND_BACK, gl.POINT)
+		case LineTriangleMode:
+			gl.PolygonMode(gl.FRONT_AND_BACK, gl.LINE)
+		case TriangleTriangleMode:
+			gl.PolygonMode(gl.FRONT_AND_BACK, gl.FILL)
+		default:
+			panic("tried to apply a render state with an unknown polygonmode")
+		}
+		currentState.TriangleMode = rs.TriangleMode
+	}
+
+	if currentState.ViewportWidth != rs.ViewportWidth && currentState.ViewportHeight != rs.ViewportHeight {
+		gl.Viewport(0, 0, int32(rs.ViewportWidth), int32(rs.ViewportHeight))
+		currentState.ViewportWidth = rs.ViewportWidth
+		currentState.ViewportHeight = rs.ViewportHeight
+	}
 }
 
 func init() {
 	gl.Enable(gl.BLEND)
+
+	// initialize cached state to default OpenGL values TODO: run apply with it?
+	currentState.Program = nil
+	currentState.Framebuffer = nil
+	currentState.DepthTest = AlwaysDepthTest
+	currentState.DisableBlending()
+	currentState.ViewportWidth, currentState.ViewportHeight = window.Size()
+	currentState.Cull = CullNothing
+	currentState.TriangleMode = TriangleTriangleMode
 }
