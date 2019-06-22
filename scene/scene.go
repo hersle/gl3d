2eb8a70a73659b009c78cf52a312f5c8cc6e1a8b
Herman Sletmoen <hermansletmoen@gmail.com>
2019-06-22 19:19:31 +0200

    Added basic scene graph tree structure

--- scene/scene.go
+++ scene/scene.go
@@ -2,10 +2,10 @@ package scene
 
 import (
 	"github.com/hersle/gl3d/light"
-	"github.com/hersle/gl3d/math"
 	"github.com/hersle/gl3d/object"
 	"github.com/hersle/gl3d/utils"
 	"image"
+	"strings"
 )
 
 type CubeMap struct {
@@ -24,6 +24,15 @@ type Scene struct {
 	PointLights       []*light.PointLight
 	DirectionalLights []*light.DirectionalLight
 	Skybox            *CubeMap
+}
+
+type Node struct {
+	mesh *object.Mesh
+	ambientLight *light.AmbientLight
+	pointLight *light.PointLight
+	spotLight *light.SpotLight
+	dirLight *light.DirectionalLight
+	children []*Node
 }
 
 func ReadCubeMap(filename1, filename2, filename3, filename4, filename5, filename6 string) (*CubeMap, error) {
@@ -54,36 +63,88 @@ func NewCubeMap(posx, negx, posy, negy, posz, negz image.Image) *CubeMap {
 	return &cm
 }
 
-func NewScene() *Scene {
-	var s Scene
-	var err error
-	if err != nil {
-		panic(err)
+func NewScene() *Node {
+	var n Node
+	return &n
+}
+
+func (n *Node) Traverse(f func(n *Node, depth int)) {
+	// depth first search
+	var nodeStack []*Node = []*Node{n}
+	var depthStack []int = []int{0};
+
+	for len(nodeStack) > 0 {
+		// pop
+		n1 := nodeStack[len(nodeStack)-1]
+		depth := depthStack[len(depthStack)-1]
+		nodeStack = nodeStack[:len(nodeStack)-1]
+		depthStack = depthStack[:len(depthStack)-1]
+
+		f(n1, depth)
+
+		for _, n2 := range n1.children {
+			nodeStack = append(nodeStack, n2)
+			depthStack = append(depthStack, depth + 1)
+		}
 	}
-	s.AmbientLight = light.NewAmbientLight(math.Vec3{0, 0, 0})
-	return &s
 }
 
-func (s *Scene) AddMesh(m *object.Mesh) {
-	s.Meshes = append(s.Meshes, m)
+func (n *Node) addNode(n2 *Node) {
+	n.children = append(n.children, n2)
 }
 
-func (s *Scene) AddAmbientLight(l *light.AmbientLight) {
-	s.AmbientLight.Color = s.AmbientLight.Color.Add(l.Color)
+func (n *Node) AddMesh(mesh *object.Mesh) {
+	var n2 Node
+	n2.mesh = mesh
+	n.addNode(&n2)
 }
 
-func (s *Scene) AddPointLight(l *light.PointLight) {
-	s.PointLights = append(s.PointLights, l)
+func (n *Node) AddAmbientLight(l *light.AmbientLight) {
+	var n2 Node
+	n2.ambientLight = l
+	n.addNode(&n2)
 }
 
-func (s *Scene) AddSpotLight(l *light.SpotLight) {
-	s.SpotLights = append(s.SpotLights, l)
+func (n *Node) AddPointLight(l *light.PointLight) {
+	var n2 Node
+	n2.pointLight = l
+	n.addNode(&n2)
 }
 
-func (s *Scene) AddDirectionalLight(l *light.DirectionalLight) {
-	s.DirectionalLights = append(s.DirectionalLights, l)
+func (n *Node) AddSpotLight(l *light.SpotLight) {
+	var n2 Node
+	n2.spotLight = l
+	n.addNode(&n2)
 }
 
-func (s *Scene) AddSkybox(skybox *CubeMap) {
-	s.Skybox = skybox
+func (n *Node) AddDirectionalLight(l *light.DirectionalLight) {
+	var n2 Node
+	n2.dirLight = l
+	n.addNode(&n2)
+}
+
+func (n *Node) String() string {
+	str := ""
+
+	f := func(n *Node, depth int) {
+		str += strings.Repeat("  ", depth) + "+ "
+		if n.mesh != nil {
+			str += "mesh"
+		} else if n.ambientLight != nil {
+			str += "ambient"
+		} else if n.pointLight != nil {
+			str += "point"
+		} else if n.spotLight != nil {
+			str += "spot"
+		} else if n.dirLight != nil {
+			str += "dir"
+		} else {
+			str += "node"
+		}
+		str += "\n"
+	}
+
+	n.Traverse(f)
+
+	return str
 }
---
+++ scene/scene_test.go
@@ -0,0 +1,15 @@
+package scene
+
+import (
+	"testing"
+	"github.com/hersle/gl3d/light"
+	"github.com/hersle/gl3d/math"
+)
+
+func TestString(t *testing.T) {
+	n := NewScene()
+	n.AddAmbientLight(light.NewAmbientLight(math.Vec3{0, 0, 0}))
+	n.AddAmbientLight(light.NewAmbientLight(math.Vec3{0, 0, 0}))
+	n.children[0].AddAmbientLight(light.NewAmbientLight(math.Vec3{0, 0, 0}))
+	println(n.String())
+}
